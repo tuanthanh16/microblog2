6b2a7478615d9b4790c33fa54b8c2d3e9ad8fb5f
Tuan Vu <111376142+tuanthanh16@users.noreply.github.com>
2026-07-06 10:16:34 +0200

    change sqlite3 to SQLalchemy

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
 import datetime
-import sqlite3
 from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "the-secret-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///microblog.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# check if database exists, if not create
+with app.app_context():
+    if not os.path.exists("microblog.db"):
+        db.create_all()
+
+# model the Task database
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.content}>'
+
+
 
 # Create a SQLite database and table
 # data.db will be created in Google Clound Bucket => each app needs different database name
 # or have to create different bucket
-conn = sqlite3.connect('data/microblog2.db')
-cursor = conn.cursor()
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS entries (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        content TEXT,
-        date TEXT
-    )
-''')
-conn.commit()
-conn.close()
+# conn = sqlite3.connect('data/microblog2.db')
+# cursor = conn.cursor()
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS entries (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         content TEXT,
+#         date TEXT
+#     )
+# ''')
+# conn.commit()
+# conn.close()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -30,27 +52,23 @@ def home():
                 return redirect(url_for('home'))
             else:     
                 formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-                # Insert user into the database
-                conn = sqlite3.connect('data/microblog2.db')
-                cursor = conn.cursor()
-                cursor.execute('INSERT INTO entries (content, date) VALUES (?, ?)', (entry_content, formatted_date))
-                conn.commit()
-                conn.close()
+                post = Post(content=entry_content, date=formatted_date)
+                db.session.add(post)
+                db.session.commit()
+
                 return redirect(url_for('home'))
         # else GET method, read data
-        conn = sqlite3.connect('data/microblog2.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM entries')
-        entries = cursor.fetchall()
+        
+        entries = Post.query.all()
         for e in entries:
-             print(e[0], e[1], e[2])
+             print(e.content, e.date)
         
         entries_with_date = [
             (
-                entry[0], #entry[0] = id
-                entry[1], #entry[1] = content
-                entry[2], #entry[2] = datetime
-                datetime.datetime.strptime(entry[2], "%Y-%m-%d").strftime("%b %d")
+                entry.id, #entry[0] = id
+                entry.content, #entry[1] = content
+                entry.date, #entry[2] = datetime
+                datetime.datetime.strptime(entry.date, "%Y-%m-%d").strftime("%b %d")
             )
             for entry in entries
         ]
@@ -59,25 +77,16 @@ def home():
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
 def edit_entry(id):
     
-    conn = sqlite3.connect('data/microblog2.db')
-    cursor = conn.cursor()
+    entry = Post.query.get_or_404(id)
 
     if request.method == "POST":
         new_content = request.form.get("content")
 
-        cursor.execute(
-            "UPDATE entries SET content = ? WHERE id = ?",
-            (new_content, id),
-        )
-        conn.commit()
-        conn.close()
-
+        entry.content = new_content
+        db.session.commit()
         return redirect(url_for("home"))
 
     # GET request → load existing entry
-    cursor.execute("SELECT id, content FROM entries WHERE id = ?", (id,))
-    entry = cursor.fetchone()
-    conn.close()
 
     return render_template("edit.html", entry=entry)
 
@@ -85,11 +94,9 @@ def edit_entry(id):
 @app.route("/delete/<int:id>")
 def delete_entry(id):
     
-    conn = sqlite3.connect('data/microblog2.db')
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM entries WHERE id = ?", (id,))
-    conn.commit()
-    conn.close()
+    entry = Post.query.get_or_404(id)
+    if entry:
+        db.session.delete(entry)
 
     return redirect(url_for("home"))
 
